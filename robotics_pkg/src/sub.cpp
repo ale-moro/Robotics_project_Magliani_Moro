a09801fb72d0efaed89a2cbed918951dfd434efe
Alessandra Moro <79703541+ale-moro@users.noreply.github.com>
2021-05-11 12:10:16 +0200

    Update sub.cpp

--- robotics_pkg/src/sub.cpp
+++ robotics_pkg/src/sub.cpp
@@ -1,10 +1,29 @@
+
 #include "ros/ros.h"
 #include "std_msgs/String.h"
 #include <nav_msgs/Odometry.h>
 #include "robotics_pkg/customOdometry.h"
 
-void subCallback(const nav_msgs::Odometry::ConstPtr& msg){
-ROS_INFO("I heard: [%d]", msg->Odometry);
+
+typedef struct odometry_values{
+    double x;
+    double y;
+    double theta;
+    double v_x;
+    double v_y;
+    double omega;
+    double steer;
+    double v_r;
+    double v_l;
+} OdometryValues;
+
+
+void Callback(const nav_msgs::Odometry::ConstPtr& msg){
+
+
+    ROS_INFO("I heard: [%d, %d]", msg->Odometry);
+	
+  
 }
 
 int main(int argc, char **argv){
@@ -12,7 +31,7 @@ int main(int argc, char **argv){
 ros::init(argc,argv,"sub_eseg");
 ros::NodeHandle n;
 
-ros::Subscriber sub = n.subscribe("Odometry",1000,subCallback);
+ros::Subscriber sub = n.subscribe("Odometry",1000,Callback);
 ros::spin();
 return 0;
 
